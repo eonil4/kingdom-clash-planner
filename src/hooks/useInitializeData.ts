d2780ea861b81b501154ad247bfffa28706d4f98
zsolt_takacs <eonil4@gmail.com>
2025-11-08 18:30:32 +0100

    feat: initialize unit roster as empty by default
    - Remove pre-population of mock units on app initialization
    - Unit roster now starts empty, requiring users to add units via Manage Units
    - Removed unused allMockUnits import from useInitializeData hook
    - Users can now build their roster from scratch

--- src/hooks/useInitializeData.ts
+++ src/hooks/useInitializeData.ts
@@ -2,13 +2,14 @@ import { useEffect } from 'react';
 import { useAppDispatch } from '../store/hooks';
 import { setUnits } from '../store/reducers/unitSlice';
 import { setFormations, setCurrentFormation } from '../store/reducers/formationSlice';
-import { allMockUnits, mockFormations } from '../utils/mockData';
+import { mockFormations } from '../utils/mockData';
 
 export function useInitializeData() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(setUnits(allMockUnits));
+    // Initialize with empty unit roster
+    dispatch(setUnits([]));
     dispatch(setFormations(mockFormations));
     if (mockFormations.length > 0) {
       dispatch(setCurrentFormation(mockFormations[0]));
